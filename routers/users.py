7532df1c6c8ca11b775738fe51a47f847368e5cb
perfectgbakidz <122876271+perfectgbakidz@users.noreply.github.com>
2025-07-06 04:42:37 +0100

    Update users.py

--- routers/users.py
+++ routers/users.py
@@ -2,37 +2,33 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
 from models import User
-from schemas import UserCreate, UserUpdate, User
+from schemas import User, UserUpdate
+from dependencies import get_current_admin, get_current_user
 
 router = APIRouter()
 
-@router.get("/", response_model=list[User])
+@router.get("/", response_model=list[User], dependencies=[Depends(get_current_admin)])
 def get_users(db: Session = Depends(get_db)):
     return db.query(User).all()
 
-@router.post("/", response_model=User)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = User(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
-
-@router.put("/{id}", response_model=User)
-def update_user(id: int, user: UserUpdate, db: Session = Depends(get_db)):
-    db_user = db.query(User).get(id)
-    if not db_user:
+@router.put("/{id}", response_model=User, dependencies=[Depends(get_current_admin)])
+def update_user(id: int, user_up: UserUpdate, db: Session = Depends(get_db)):
+    user = db.query(User).get(id)
+    if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    for k, v in user.dict().items():
-        setattr(db_user, k, v)
+    if user_up.role:
+        user.role = user_up.role
+    user.name = user_up.name or user.name
+    user.department = user_up.department or user.department
     db.commit()
-    return db_user
+    db.refresh(user)
+    return user
 
-@router.delete("/{id}")
+@router.delete("/{id}", dependencies=[Depends(get_current_admin)])
 def delete_user(id: int, db: Session = Depends(get_db)):
-    db_user = db.query(User).get(id)
-    if not db_user:
+    user = db.query(User).get(id)
+    if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    db.delete(db_user)
+    db.delete(user)
     db.commit()
     return {"detail": "User deleted"}
