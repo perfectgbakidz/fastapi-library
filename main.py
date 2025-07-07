f9691b7b92e5855b045e63c2aa1dfa23407159ad
perfectgbakidz <122876271+perfectgbakidz@users.noreply.github.com>
2025-07-07 13:58:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from fastapi import FastAPI
-from routers import books, users, loans, dashboard
 from database import Base, engine
+from routers import books, users, loans, dashboard, auth
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Library Management API")
+
+app = FastAPI(title="Library Management API (Secure)")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or specify your frontend URL instead of "*"
+    allow_origins=["*"],  # or specify frontend origins like ["http://localhost:5500"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Include routers
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
 app.include_router(books.router, prefix="/books", tags=["Books"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
